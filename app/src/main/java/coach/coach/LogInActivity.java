7ce525a2c6487653929c825d83c8c4464b069a70
alonspivack1 <alonspivack1@gmail.com>
2019-02-02 16:33:32 +0200

    lastt

--- app/src/main/java/coach/coach/LogInActivity.java
+++ app/src/main/java/coach/coach/LogInActivity.java
@@ -1,23 +1,109 @@
 package coach.coach;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.firebase.FirebaseError;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
 
 public class LogInActivity extends AppCompatActivity {
+    private DatabaseReference databaseReference;
+    String Answer,x="bb";
+    int pl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
-        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
 
-        myRef.setValue("Hello, World!");
 
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //Map<String, Object> objectMap = (HashMap<String, Object>) dataSnapshot.child("Users").child(user.getUid()).child("username").getValue();
+                Map<String, Object> objectMap = (HashMap<String, Object>) dataSnapshot.child("Names").getValue();
+                Answer = objectMap.toString();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+       /* mDatabase = FirebaseDatabase.getInstance().getReference();
+        String username= "cc" ;
+        mDatabase.child("Names").equalTo(username).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot!=null && dataSnapshot.getChildren()!=null &&
+                        dataSnapshot.getChildren().iterator().hasNext()){
+                    Log.e("exists","exists");
+                    //Username exists
+                }
+                else {
+                    //Username does not exist
+                    Log.e("not exist","not exist");
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+
+
+        });*/
+
+       /* String myString = "Aa1 90";
+        if(Pattern.matches("[A-Za-z0-9]+", myString))
+        {
+            Toast.makeText(this,"string has only alphabets",Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            Toast.makeText(this,"It has other characters",Toast.LENGTH_LONG).show();
+        }*/
+
+                /*Intent intent = new Intent (this, SignUpActivity.class);
+                 startActivity(intent);*/
     }
 
+    public void AAAAAA(View view) {
+        //Toast.makeText(this,Answer,Toast.LENGTH_LONG).show();
+
+        pl =Answer.indexOf("=, "+x);
+        if(pl==-1)
+        {
+            Toast.makeText(this,Answer+"Can be username"+pl,Toast.LENGTH_LONG).show();
+            Log.e("keys",Answer);
+        }
+        else
+        {
+            Toast.makeText(this,Answer+"Cant be username"+pl,Toast.LENGTH_LONG).show();
+            Log.e("keys",Answer);
+
+        }
+
+
+    }
 }
--- app/src/main/java/coach/coach/SignUpActivity.java
+++ app/src/main/java/coach/coach/SignUpActivity.java
@@ -1,13 +1,192 @@
 package coach.coach;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class SignUpActivity extends AppCompatActivity {
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ProgressBar progressBar;
+    EditText editTextEmail, editTextPassword,editTextUser;
+    DatabaseReference reference,reference2;
+    int pl;
+    private FirebaseAuth mAuth;
+    private DatabaseReference databaseReference;
+    String Answer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        FirebaseDatabase reference;
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        editTextUser = (EditText)findViewById(R.id.editTextUser);
+        progressBar = (ProgressBar) findViewById(R.id.progressbar);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        findViewById(R.id.buttonSignUp).setOnClickListener(this);
+        findViewById(R.id.textViewLogin).setOnClickListener(this);
+        setContentView(R.layout.activity_log_in);
+
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //Map<String, Object> objectMap = (HashMap<String, Object>) dataSnapshot.child("Users").child(user.getUid()).child("username").getValue();
+                Map<String, Object> objectMap = (HashMap<String, Object>) dataSnapshot.child("Names").getValue();
+                Answer = objectMap.toString();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void registerUser() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        final String username = editTextUser.getText().toString();
+        if (email.isEmpty()) {
+            editTextEmail.setError("Email is required");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Please enter a valid email");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            editTextPassword.setError("Password is required");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if (password.length() < 6) {
+            editTextPassword.setError("Minimum lenght of password should be 6");
+            editTextPassword.requestFocus();
+            return;
+        }
+        if (username.length() < 2) {
+            editTextUser.setError("Minimum lenght of username should be 2");
+            editTextUser.requestFocus();
+            return;
+        }
+        if (username.length() > 10) {
+            editTextUser.setError("Max lenght of username should be 10");
+            editTextUser.requestFocus();
+            return;
+        }
+        if(!Pattern.matches("[A-Za-z0-9]+", username))
+        {
+            editTextUser.setError("please use just letters and numbers");
+            editTextUser.requestFocus();
+            return;
+        }
+
+        pl =Answer.indexOf("=, "+username);
+        if(pl!=-1)
+        {
+            editTextUser.setError("This name is taken, please choose other name");
+            editTextUser.requestFocus();
+            return;
+        }
+
+
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressBar.setVisibility(View.GONE);
+                if (task.isSuccessful()) {
+                    FirebaseUser firebaseUser = mAuth.getCurrentUser();
+                    String userid = firebaseUser.getUid();
+                    reference2 = FirebaseDatabase.getInstance().getReference("Names");
+                    HashMap<String, String> hashMap2 = new HashMap<>();
+                    hashMap2.put(username,"");
+                    reference2.setValue(hashMap2).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            task.isComplete();
+                        }
+                    });
+                    /*reference = FirebaseDatabase.getInstance().getReference("Users").child(userid);
+                    HashMap<String, String> hashMap = new HashMap<>();
+                    hashMap.put("id",userid);
+                    hashMap.put("username",username);
+                    reference.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isComplete()){
+                                startActivity(new Intent(SignUpActivity.this, ProfileActivity.class));
+                            }
+                        }
+                    });*/
+                    finish();
+
+                } else {
+
+                    if (task.getException() instanceof FirebaseAuthUserCollisionException) {
+                        Toast.makeText(getApplicationContext(), "You are already registered", Toast.LENGTH_SHORT).show();
+
+                    } else {
+                        Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.buttonSignUp:
+                registerUser();
+                break;
+
+            case R.id.textViewLogin:
+                finish();
+                startActivity(new Intent(this, LogInActivity.class));
+                break;
+        }
     }
 }
